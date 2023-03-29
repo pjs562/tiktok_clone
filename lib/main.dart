eb0200eefc3ed15aeb131f7e578d223a7d502ae9
Joonseo Park <jspark2015@unist.ac.kr>
2023-03-29 20:32:57 +0900

    Add BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class TikTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           )),
-      home: const MainNavigationScreen(),
+      home: MainNavigationScreen(),
     );
   }
 }
