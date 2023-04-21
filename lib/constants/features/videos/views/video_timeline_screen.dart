5d50679971e87d483044d80b4cfb6c1d0af90a60
Joonseo Park <jspark2015@unist.ac.kr>
2023-04-21 20:03:04 +0900

    Add AsyncNotifierProvider

--- lib/constants/features/videos/views/video_timeline_screen.dart
+++ lib/constants/features/videos/views/video_timeline_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/constants/features/videos/view_models/timeline_view_model.dart';
 import 'package:tiktok_clone/constants/features/videos/views/widgets/video_post.dart';
 
-class VideoTimelineScreen extends StatefulWidget {
+class VideoTimelineScreen extends ConsumerStatefulWidget {
   const VideoTimelineScreen({Key? key}) : super(key: key);
 
   @override
-  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+  VideoTimelineScreenState createState() => VideoTimelineScreenState();
 }
 
-class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+class VideoTimelineScreenState extends ConsumerState<VideoTimelineScreen> {
   int _itemCount = 4;
 
   final PageController _pageController = PageController();
@@ -41,27 +43,38 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     super.dispose();
   }
 
-  Future<void> _onRefresh(){
-    return Future.delayed(
-        const Duration(seconds: 2)
-    );
+  Future<void> _onRefresh() {
+    return Future.delayed(const Duration(seconds: 2));
   }
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      displacement: 70,
-      backgroundColor: Colors.black87,
-      color: Theme.of(context).primaryColor,
-      onRefresh: _onRefresh,
-      child: PageView.builder(
-        controller: _pageController,
-        scrollDirection: Axis.vertical,
-        onPageChanged: _onPageChanged,
-        itemCount: _itemCount,
-        itemBuilder: (BuildContext context, int index) =>
-            VideoPost(onVideoFinished: _onVideoFinished, index: index),
-      ),
+    return ref.watch(timelineProvider).when(
+        loading: () =>
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+        error: (error, stackTrace) =>
+            Center(
+              child: Text('Could not load videos: $error',
+              style: TextStyle(color: Colors.white),),
+            ),
+        data: (videos) => RefreshIndicator(
+          displacement: 70,
+          backgroundColor: Colors.black87,
+          color: Theme
+              .of(context)
+              .primaryColor,
+          onRefresh: _onRefresh,
+          child: PageView.builder(
+            controller: _pageController,
+            scrollDirection: Axis.vertical,
+            onPageChanged: _onPageChanged,
+            itemCount: videos.length,
+            itemBuilder: (BuildContext context, int index) =>
+                VideoPost(onVideoFinished: _onVideoFinished, index: index),
+          ),
+        ),
     );
   }
 }
